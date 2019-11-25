c93e1627eed8d32458cd7b7c5a45ee971be37eab
bcht201 <brianting1227@gmail.com>
2019-11-25 11:47:40 +0000

    Add basic write to JSON functionality

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const fs = require('fs');
 
 app.use('/', express.static('public'));
 app.use('/', express.static('views'));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.send('index')
@@ -11,6 +13,24 @@ app.get('/', (req, res) => {
 
 app.get('/newPost', (req, res) => {
   res.sendFile(__dirname + '/views/newPost.html')
+});
+
+app.post('/publishPost', (req, res)=>{
+  const blogAuthor = req.body.blogAuthor;
+  const blogTitle = req.body.blogTitle;
+  const blogContent = req.body.blogContent;
+  const blogDate = new Date(Date.now());
+  console.log(blogDate);
+  const data = {
+    blogAuthor,
+    blogTitle,
+    blogContent,
+    blogDate
+  }
+  // fs.writeFile('blogPost.json', JSON.stringify(data), (error)=>{
+  //   if(error) throw error;
+  //   console.log("Data written to file.");
+  // });
 });
 
 app.listen(port, () => console.log('Listening on 8080'));
