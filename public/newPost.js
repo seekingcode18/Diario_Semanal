a2caf20f02e6b0a536c5af9126cc2950ac58cc66
bcht201 <brianting1227@gmail.com>
2019-11-26 16:10:54 +0000

    Display gifs on new post page after clicking search button

--- public/newPost.js
+++ public/newPost.js
@@ -1,9 +1,17 @@
 $(document).ready(()=>{
-  const giphyUrl = 'https://api.giphy.com/v1/gifs/search';
-  const apiKey = 'VFHmiyXHL7mnADRp3qP3sF0Psi3LexUD';
-  let apiCall = `${giphyUrl}?q=cat&api_key=${apiKey}`;
-  fetch(apiCall)
-    .then(res => res.text())
-    .then(res => console.log(res))
-
-})
+  $('#gifSubmit').click(event =>{
+    event.preventDefault();
+    const gifSearch = $('#gifSearch').val();
+    const giphyUrl = 'https://api.giphy.com/v1/gifs/search';
+    const apiKey = 'VFHmiyXHL7mnADRp3qP3sF0Psi3LexUD';
+    let apiCall = `${giphyUrl}?q=${gifSearch}&api_key=${apiKey}&limit=8`;
+    fetch(apiCall)
+      .then(res => res.text())
+      .then(res =>{
+        let gifList = JSON.parse(res).data.map(gif => gif.images.fixed_height_downsampled.url);
+        gifList.forEach(gifURL => {
+          $('.gifs__display').append(`<img src=${gifURL}>`);
+        });
+    })
+  })
+});
