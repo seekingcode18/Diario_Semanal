8010e31af4878c060d27a62f409bc3e7c9db64a7
Christopher <christopher@seekingwebdev.com>
2019-11-27 10:52:02 +0000

    try adding alt attributes to gifs on new post page and blog post page

--- public/newPost.js
+++ public/newPost.js
@@ -12,10 +12,16 @@ $(document).ready(() => {
       fetch(apiCall)
         .then(res => res.text())
         .then(res => {
-          console.log(res);
-          let gifList = JSON.parse(res).data.map(gif => gif.images.fixed_height_downsampled.url);
+          console.log('before', JSON.parse(res).data);
+          let gifList = JSON.parse(res).data.map(gif => {
+            return gifData = {
+              title: gif.title,
+              url: gif.images.fixed_height_downsampled.url
+            };
+          });
+          console.log('after', gifList);
           gifList.forEach(gifURL => {
-            $('.gifs__display').append(`<div class="gif__wrapper"><img src=${gifURL}></div>`);
+            $('.gifs__display').append(`<div class="gif__wrapper"><img src=${gifURL.url} alt=${gifURL.title}></div>`);
           });
         })
         //loops through all of the images after they've been dislayed and appended, and listens for a click 
